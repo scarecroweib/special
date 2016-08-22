a678ff3dd7b4ef73b65460c23bbf3abddbde7bb6
scarecroweib <scarecroweib@gmail.com>
2016-08-22 11:45:17 +0800

    添加静态文件的处理

--- src/main/java/com/weib/special/configuration/WebConfig.java
+++ src/main/java/com/weib/special/configuration/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -84,5 +85,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         templateResolver.setCharacterEncoding("UTF-8");
         templateResolver.setCacheable(false);
         return templateResolver;
+    }
+    
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
     }
 }
