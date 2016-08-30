9315580c17ac53779122c1be635c0a143036db1d
scarecroweib <scarecroweib@gmail.com>
2016-08-30 11:46:22 +0800

    ContextLoaderListener及DispatcherServlet的配置及web.xml对应关系注释

--- src/main/java/com/weib/special/configuration/SpecialWebInitializer.java
+++ src/main/java/com/weib/special/configuration/SpecialWebInitializer.java
@@ -5,27 +5,59 @@
  */
 package com.weib.special.configuration;
 
+import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
- *
+ * 对应web.xml的设置
  * @author zhangjingwei
  */
 public class SpecialWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    /**
+     * 对应web.xml中的通用Bean配置 ContextLoaderListener
+     * @return 
+     */
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{ RootConfig.class };
     }
 
+    /**
+     * 对应web.xml中的DispatcherServlet配置
+     * @return 
+     */
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{ WebConfig.class };
     }
 
+    /**
+     * 对应web.xml中的servlet-mapping
+     * @return 
+     */
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+    
+    /**
+     * 对应web.xml中DispatcherServlet的自定义配置
+     * @param registration 
+     */
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setLoadOnStartup(1);
+        registration.setMultipartConfig(new MultipartConfigElement("F:\\var\\tmp"));
+    }
+    
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{};
+    }
+    
+    
     
 }
